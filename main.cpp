3d746e4f7040327798697c81a1b3ea39f35d6595
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2017-04-20 13:02:36 +0100

    made the cin run multiple times

--- main.cpp
+++ main.cpp
@@ -7,10 +7,15 @@ void PrintIntro(int WORD_LENGTH);
 string PrintGuessAndPrintBack(string Guess);
 
 int main() {
-	PrintIntro(5);
-	string Guess;
-	//get a guess from the user
-	PrintGuessAndPrintBack(Guess);
+	constexpr int WORD_LENGTH = 5;
+	PrintIntro(WORD_LENGTH);
+	string Guess = "";
+
+	//loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+		PrintGuessAndPrintBack(Guess);
+
 	return 0;
 }
 
@@ -24,6 +29,7 @@ void PrintIntro(int WORD_LENGTH) {
 }
 
 string PrintGuessAndPrintBack(string Guess){
+	//get a guess from the user
 	cout << "Enter your guess: ";
 	getline(cin, Guess);
 
